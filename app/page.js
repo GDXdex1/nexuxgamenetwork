03c2bf8571649247b89823a6386ca75d20138d3b
GDXdex1 <jhonnycarpio15@gmail.com>
2025-10-20 14:41:44 -0400

    feat: plataforma P2E en JavaScript puro

---
+++ app/page.js
@@ -0,0 +1,16 @@
+{
+  "name": "gaudix-p2e",
+  "version": "0.1.0",
+  "private": true,
+  "type": "module",
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start"
+  },
+  "dependencies": {
+    "next": "14.2.3",
+    "react": "18",
+    "react-dom": "18"
+  }
+}
